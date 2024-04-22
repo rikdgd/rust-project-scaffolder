0048eacc752152b8d98ca980f31aad6043a2e075
Rik de Goede <rik.dgd@gmail.com>
2024-04-22 20:00:26 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+mod project_generator;
+
+use std::env;
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::from_args(args);
+    
+    match config {
+        Some(config) => {
+            println!("Target project: {}", config.target_project);
+        },
+        None => {
+            println!("Wrong amount of arguments provided!");
+        },
+    }
+}
+
+
+
+struct Config {
+    target_project: String,
+}
+
+impl Config {
+    fn from_args(args: Vec<String>) -> Option<Config> {
+        match args.len() {
+            1 => None, 
+            2 => Some(Config {target_project: String::from(&args[1])}),
+            _ => None,
+        }
+    }
+}
